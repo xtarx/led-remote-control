386e41ddf01d22a0bf099b7b90fc14686d0291e5
Mihai Baltac <mbaltac@mbaltac-mbp2017.local>
2017-04-06 15:54:03 +0200

    screen

---
+++ sketch_arduion_screen/sketch_arduion_screen.ino
@@ -0,0 +1,161 @@
+// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
+// released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1
+#define PIN            13
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      180 //76 //real count is 57
+
+
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_RGBW+NEO_KHZ800);
+uint32_t green = pixels.Color(255, 0, 0,0);
+uint32_t red = pixels.Color(0, 255, 0,0);
+uint32_t blue = pixels.Color(0, 0, 255,0);
+uint32_t off = pixels.Color(0, 0, 0,0);
+
+int delayval = 50; // delay for half a second
+
+
+
+void setPixel(int x, int y, uint32_t color) {
+  if (x==0) {
+    pixels.setPixelColor(y, color);
+  } else if (x==1) {
+    pixels.setPixelColor(119-y, color);
+  } else if (x==2) {
+    pixels.setPixelColor(120+y, color);
+  }
+}
+
+void setRange(int starting_index, int offset, uint32_t color) {
+
+    for(int i=starting_index;i<offset+1;i++){
+      setPixel(0, i, color);
+      setPixel(1, i, color); 
+      setPixel(2, i, color); 
+  }
+}
+
+void setRangeRow(int row, int starting_index, int offset, uint32_t color) {
+
+    for(int i=starting_index;i<offset+1;i++){
+      setPixel(row, i, color); 
+  }
+}
+
+
+
+
+
+
+void setArrowRight(int index, uint32_t color) {
+  for (int i=index; i < index + 5; i++) {
+    setPixel(1, i, color);   
+  }
+  setPixel(0, index+3, color);
+  setPixel(2, index+3, color);
+  
+  
+}
+
+void setArrowLeft(int index, uint32_t color) {
+  for (int i=index; i < index+5; i++) {
+    setPixel(1, i, color);   
+  }
+  setPixel(0, index+1, color);
+  setPixel(2, index+1, color);
+}
+
+void setArrowRight1(int index, uint32_t color) {
+  setRange(index, index, color);
+  setPixel(1, index+1, color);
+}
+
+void setArrowLeft1(int index, uint32_t color) {
+  setRange(index+1, index+1, color);
+  setPixel(1, index, color);
+}
+
+
+void door(int index, uint32_t color) {
+  setRangeRow(0, index, index+4, color);
+  setRangeRow(1, index+1, index+3, color);
+  setPixel(2, index+2, color);
+}
+
+void greenDoorBlinking(int index) {
+  for(int i=0; i<3; i++) {
+    setRange(index, index+8, pixels.Color(0, 0, 0,1));
+    door(index+2, blue);
+    setArrowRight1(index+i, green);
+    setArrowLeft1(index+7-i, green);
+    pixels.show();
+    delay(200);
+  }
+}
+
+void greenDoorStatic(int index) {
+  setRange(index, index, pixels.Color(255, 0, 0,200));
+  setRange(index+1, index+1, pixels.Color(255, 0, 0,100));
+  setArrowRight1(index+2, green);
+  door(index+3, blue);
+  setArrowLeft1(index+5, green);
+  setRange(index+6, index+6, pixels.Color(255, 0, 0,100));
+  setRange(index+7, index+7, pixels.Color(255, 0, 0,200));
+  pixels.show();
+}
+
+void ex(int index, uint32_t color) {
+  setPixel(0, index, color);
+  setPixel(2, index, color);
+  setPixel(1, index+1, color);
+  setPixel(0, index+2, color);
+  setPixel(2, index+2, color);
+}
+
+void staticLights(){
+  setRange(0, NUMPIXELS, pixels.Color(0, 0, 0,1));
+//  greenDoorStatic(20);
+  ex(20, red);
+  ex(50, red);
+  door(5, green);
+  door(35, green);
+}
+
+void setup() {
+  Serial.print("test");
+
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+  // End of trinket special code
+
+  pixels.begin(); // This initializes the NeoPixel library.
+  staticLights();
+  pixels.show();
+  pixels.setBrightness(120);
+
+}
+
+void repeat(){
+  greenDoorBlinking(5);
+  greenDoorBlinking(35);
+  
+}
+
+void loop() {
+//  repeat();
+
+}
